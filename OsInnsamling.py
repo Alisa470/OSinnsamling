24467e2e7b14256f064567f34aa264d1f6bbb678
Alisa Damashyts <alisada@bfk.no>
2024-11-26 13:42:48 +0100

    Added 'var' in the beggining of every variable

--- OsInnsamling.py
+++ OsInnsamling.py
@@ -19,9 +19,9 @@ def func_GetOSandVersion():
 
 def func_GetFreeDiskSpace():
     try:
-        infoDisk = psutil.disk_usage("/")
+        var_infoDisk = psutil.disk_usage("/")
         return (f"Ledig plass på disk: "
-                f"{round(infoDisk.free / (1024 ** 3), 2)} GB"
+                f"{round(var_infoDisk.free / (1024 ** 3), 2)} GB"
                 )
     except Exception as fail:
         print("Noe gikk galt...")
@@ -50,18 +50,19 @@ def func_GetIPaddress():
 def func_GetInstalledSW():
     try:
         if platform.system() == "Windows":
-            cmd = ("powershell "
-                   "'Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\"
-                   "CurrentVersion\\Uninstall\\* | Select-Object DisplayName'"
-                   )
+            var_cmd = ("powershell "
+                       "'Get-ItemProperty HKLM:\\Software\\Microsoft"
+                       "\\Windows\\CurrentVersion\\Uninstall\\* "
+                       "| Select-Object DisplayName'"
+                       )
         elif platform.system() == "Linux":
-            cmd = "dpkg --get-selections"
+            var_cmd = "dpkg --get-selections"
         elif platform.system() == "Darwin":
-            cmd = "ls /Applications"
+            var_cmd = "ls /Applications"
         else:
             return "Din OS støttes ikke"
         return (f"Installert programvare: "
-                f"{subprocess.check_output(cmd, shell=True, text=True)}"
+                f"{subprocess.check_output(var_cmd, shell=True, text=True)}"
                 )
     except Exception as fail:
         print("Noe gikk galt...")
